7ff1b7883c69a50c7ae76963cdb3f347d97dacae
Felipe Eulalio <Felipe1@MacBook-Pro-de-Felipe.local>
2015-03-24 11:16:39 -0300

    Adicionando a conectividade

---
+++ DoYouKnowMe/DoYouKnowMe/ConnectionsViewController.h
@@ -0,0 +1,13 @@
+//
+//  ConnectionsViewController.h
+//  DoYouKnowMe
+//
+//  Created by Felipe Eulalio on 24/03/15.
+//  Copyright (c) 2015 Arthur Alvarez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ConnectionsViewController : UIViewController
+
+@end

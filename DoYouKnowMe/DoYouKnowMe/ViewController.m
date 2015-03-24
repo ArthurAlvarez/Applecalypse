70fde10853b7610cfce9216d43eeb68bc0826722
Arthur Alvarez <arthuralvarez@Arthurs-MacBook-Pro.local>
2015-03-24 10:24:09 -0300

    Implementação inicial da view inicial e seu controller

--- DoYouKnowMe/DoYouKnowMe/ViewController.m
+++ DoYouKnowMe/DoYouKnowMe/ViewController.m
@@ -10,6 +10,9 @@
 
 @interface ViewController ()
 
+/// Interface button in the initial screen. (Screen label: 'Jogar')
+@property (weak, nonatomic) IBOutlet UIButton *btn_StartApp;
+
 @end
 
 @implementation ViewController
@@ -22,6 +25,15 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+/**
+ Starts the application from the initial view. This method is called when the user presses 'btn_StartButton'.
+ @param sender: The object that called the method
+ @author Arthur Alvarez
+ */
+- (IBAction)startApp:(id)sender {
+    //TO DO
 }
 
 @end

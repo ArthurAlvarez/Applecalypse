275d9422faf634fe8c90600b8bb8de39da8b2710
Arthur Alvarez <arthuralvarez@Arthurs-MacBook-Pro.local>
2015-03-24 09:14:24 -0300

    Criando Projeto no Xcode

---
+++ DoYouKnowMe/DoYouKnowMe/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DoYouKnowMe
+//
+//  Created by Arthur Alvarez on 3/24/15.
+//  Copyright (c) 2015 Arthur Alvarez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DoYouKnowMe/DoYouKnowMe/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DoYouKnowMe
+//
+//  Created by Arthur Alvarez on 3/24/15.
+//  Copyright (c) 2015 Arthur Alvarez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+

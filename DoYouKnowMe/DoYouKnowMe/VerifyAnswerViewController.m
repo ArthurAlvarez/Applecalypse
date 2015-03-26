7eb600fc88aba1a07777edffd118bf9fb3893b9e
Arthur Alvarez <arthuralvarez@Arthurs-MacBook-Pro.local>
2015-03-26 11:27:36 -0300

    Comunicação na tela de verificação

--- DoYouKnowMe/DoYouKnowMe/VerifyAnswerViewController.m
+++ DoYouKnowMe/DoYouKnowMe/VerifyAnswerViewController.m
@@ -7,7 +7,9 @@
 //
 
 #import "VerifyAnswerViewController.h"
+#import "Connectivity.h"
 #import "Player.h"
+#import "AppDelegate.h"
 
 @interface VerifyAnswerViewController ()
 ///Interface Button where the user Accepts the answer
@@ -17,6 +19,8 @@
 @property (weak, nonatomic) IBOutlet UIButton *btnRejectAnswer;
 
 @property (weak, nonatomic) IBOutlet UILabel *playerLabel;
+
+@property (strong, nonatomic) AppDelegate *appDelegate;
 
 @end
 
@@ -38,6 +42,14 @@
         [self.btnAcceptAnswer setHidden: YES];
         [self.btnRejectAnswer setHidden:YES];
     }
+    
+    _appDelegate = (AppDelegate *)[[UIApplication sharedApplication] delegate];
+    
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(didReceiveDataWithNotification:)
+                                                 name:@"MCDidReceiveDataNotification"
+                                               object:nil];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -50,6 +62,7 @@
 @author Arthur Alvarez
  */
 - (IBAction)acceptAnswer:(id)sender {
+    [self sendAnswer:@"1"];
     [self performSegueWithIdentifier:@"backToGame" sender:self];
 }
 
@@ -58,8 +71,41 @@
  @author Arthur Alvarez
  */
 - (IBAction)rejectAnswer:(id)sender {
+    [self sendAnswer:@"0"];
     [self performSegueWithIdentifier:@"backToGame" sender:self];
 }
+
+/**
+ This method is called when Player1 evaluates the answer and sends the result to Player2
+ */
+-(void)sendAnswer:(NSString*)strAnswer{
+    NSArray *allPeers = _appDelegate.mcManager.session.connectedPeers;
+    NSError *error;
+    NSData *dataToSend = [strAnswer dataUsingEncoding:NSUTF8StringEncoding];
+    
+    NSLog(@"Sending Data to: %@", allPeers);
+    
+    [_appDelegate.mcManager.session sendData:dataToSend
+                                     toPeers:allPeers
+                                    withMode:MCSessionSendDataReliable
+                                       error:&error];
+    
+    if (error) {
+        NSLog(@"%@", [error localizedDescription]);
+    }
+}
+
+-(void)didReceiveDataWithNotification:(NSNotification *)notification
+{
+    NSData *receivedData = [[notification userInfo] objectForKey:@"data"];
+    NSString *receivedInfo = [[NSString alloc] initWithData:receivedData encoding:NSUTF8StringEncoding];
+ 
+    NSLog(@"Received Data: %@", receivedInfo);
+    
+    if([receivedInfo isEqualToString:@"0"] || [receivedInfo isEqualToString:@"1"])
+        [self performSegueWithIdentifier:@"backToGame" sender:self];
+}
+
 
 /*
 #pragma mark - Navigation

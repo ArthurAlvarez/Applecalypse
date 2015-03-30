a9edcbd53483b07800a7b3b3d5c806bdd1f8abad
Arthur Alvarez <arthuralvarez@Arthurs-MacBook-Pro.local>
2015-03-30 10:40:51 -0300

    Criando classe GameSettings

---
+++ DoYouKnowMe/DoYouKnowMe/GameSettings.m
@@ -0,0 +1,16 @@
+//
+//  GameSettings.m
+//  DoYouKnowMe
+//
+//  Created by Arthur Alvarez on 3/30/15.
+//  Copyright (c) 2015 Arthur Alvarez. All rights reserved.
+//
+
+/**
+ Holds information about the game settings
+ */
+#import "GameSettings.h"
+
+@implementation GameSettings
+
+@end

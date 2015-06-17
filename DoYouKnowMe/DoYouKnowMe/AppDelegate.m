cc339ec8e0502028ae3971d81e702646debd8b4e
Arthur Alvarez <arthuralvarez@Arthurs-MacBook-Pro.local>
2015-06-17 11:30:16 -0300

    Correções

--- DoYouKnowMe/DoYouKnowMe/AppDelegate.m
+++ DoYouKnowMe/DoYouKnowMe/AppDelegate.m
@@ -109,9 +109,6 @@
 
 - (void)applicationWillTerminate:(UIApplication *)application {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    
-    [_mcManager.session finalize];
-    _mcManager.session = nil;
 }
 
 @end
